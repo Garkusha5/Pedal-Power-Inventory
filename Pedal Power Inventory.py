760d6e203bd54f2aa5f13d66bf4e87fb64c35bdc
Garkusha5 <37304687+Garkusha5@users.noreply.github.com>
2018-05-19 13:25:57 -0400

    Update Pedal Power Inventory.py
    Following revisions made:
    line 7: I'm not sure if you need the .iloc (removed)
    lines 13-15: True and False are of type boolean so they shouldn't be surrounded with ' ' (only strings are surrounded with quotations)
    lines 19-21: contracted into 1 line (rather than 2 lines, 1 to define variable and 1 to add column, combine to add column which is a function of existing columns), i.e.:
    inventory["total_value"] = inventory.price * inventory.quantity

--- Pedal Power Inventory.py
+++ Pedal Power Inventory.py
@@ -4,21 +4,19 @@ import pandas as pd
 inventory = pd.read_csv('inventory.csv')
 print(inventory.head(10))
 
-staten_island = inventory.iloc[:10] # first 10 columns saved as staten island.
+staten_island = inventory[:10] # first 10 columns saved as staten island.
 
 product_request = staten_island.product_description # product description from variable state_island.
 
 seed_request = inventory[(inventory.location == 'Brooklyn') & (inventory.product_type == 'seeds')]
 
-in_stock = lambda row: 'True' \
+in_stock = lambda row: True \
 	if row['quantity'] > 0 \
-  else 'False'
+  else False
   
 inventory.in_stock = inventory.apply(in_stock, axis = 1)
 
-total_value = inventory.price * inventory.quantity
-
-inventory['total_value'] = total_value
+inventory['total_value'] = inventory.price * inventory.quantity
 
 combine_lambda = lambda row: \
 	'{} - {}'.format(row.product_type,
